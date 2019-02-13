df696739793d908fbd3702d97e1c4b4a0c148e89
Yuqian Wang <iamwangyuqian@yahoo.com>
2019-02-12 22:52:28 -0500

    implement header

--- App.js
+++ App.js
@@ -1,21 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Header } from 'react-native-elements'
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+      <View style={{flex: 1, backgroundColor: '#ddd'}}>
+      <Header
+        centerComponent={{text: 'Image Search Proj', style: { color: 'white'}}}
+        />
+        {/*searchbar*/}
+        {/*imglist*/}
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+//header
+//searchbar
+//list of image => imglistitem
